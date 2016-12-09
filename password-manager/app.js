b54b5bdb094f072b0ed97d0fab0536c3761cec03
Calvin Lang <langcalvin@gmail.com>
2016-12-09 17:32:47 +0800

    The PW manager was too basic

---
+++ password-manager/app.js
@@ -0,0 +1,10 @@
+var storage = require('node-persist');
+storage.initSync();
+
+var account = storage.getItemSync(account);
+
+function createAccount(account, userAccountInformation ) {
+	account
+};
+
+console.log(account);
