1d12890419a94f805acab4155d95124012e9b891
Calvin Lang <langcalvin@gmail.com>
2016-12-15 09:24:15 +0800

    New project

---
+++ web-server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var middleware = require('./middleware.js')
+var app = express()
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication , function(req, res){
+	res.send('About Us')
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(3000, function() {
+	console.log('the server started');
+});
